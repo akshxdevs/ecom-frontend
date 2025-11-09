e6bf015e3a74ae50ef6c9a64319bd6fd08d9601f
akshxdevs <akshxdevs@gmail.com>
2025-11-09 11:39:30 +0530

    update: added banner, hearder text

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,25 @@
 import { ArrowRight, ChevronDown, ShoppingBagIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import localFont from "next/font/local"; 
+import { TokenSOL, TokenETH } from '@token-icons/react';
 
 const myFont = localFont({
     src: '../public/fonts/Palmore.otf',
 });
 
+const bannerFont = localFont({
+  src:'../public/fonts/BricolageGrotesque-VariableFont_opsz.ttf',
+});
+
+const explorebtn = localFont({
+  src:'../public/fonts/MartianMono-VariableFont_wdth,wght.ttf',
+});
 export default function Home() {
   const router = useRouter();
   return (
     <div>
-      <div className="max-w-[98%] w-full mx-auto flex justify-between items-center gap-4 py-3 ">
-        <div onClick={()=>router.push("/")} className="flex items-center gap-1">
+      <div className="max-w-[90%] w-full mx-32 flex justify-between items-center gap-4 py-3">
+        <div onClick={()=>router.push("/")} className="flex items-center">
           <ShoppingBagIcon size={20}/>
           <h1 className={`${myFont.className} hidden md:block font-bold text-3xl cursor-pointer text-white-1/2`}>blockBazzar</h1>
         </div>
@@ -43,26 +51,32 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex justify-center items-center gap-2 font-sans text-sm bg-blue-400/30">
+      <div className={`flex justify-center items-center gap-2 font-sans text-sm bg-blue-400/30`}>
         <p className="bg-blue-600 px-2 py-0.5 rounded-xl">NEW</p>
         <p className={`${myFont.className} text-2xl`}>Offer Available!</p>
         <p>Check out the new trending offers and exclusive deals</p>
         <ArrowRight size={15}/>
       </div>
-      <div>
-        // main header
-        <div>
-          // title
-        </div>
-        <div>
-          // description
-        </div>
-        <div>
-          // start btn
-        </div>
+      <div className={`${bannerFont.className} flex flex-col justify-center items-center pt-10 pb-5`}>
+        <h1 className="text-6xl font-semibold">All products</h1>
+        <h1 className="text-6xl font-semibold">in your <span className="text-6xl font-semibold">on-chain app</span></h1>
       </div>
-      <div>
-        // animation banner
+      <div className="flex justify-center items-center">
+        <h2 className="text-zinc-500 w-[40%] text-center">Access 7+ million tokens with liquidity from 130+ exchanges,with the best prices and the smoothest trading experience.</h2>
+      </div>
+      <div className="flex justify-center items-center py-5">
+        <button onClick={()=>router.push("/products")} className={`${explorebtn.className} flex items-center py-2 px-4 font-sans font-semibold text-sm text-black bg-white/95 rounded-xl`}>
+          Explore
+        </button>
+      </div>
+      <div className="flex justify-center items-center py-20 ">
+        <img src="/img/banner.png" alt="banner" className="border border-zinc-700 rounded-md h-1/2 w-1/2 object-cover"/>
+        <div className="absolute right-[22%] bottom-44 px-4 py-2 rounded-md border border-zinc-800 bg-zinc-900">
+          <TokenSOL size={40} variant="branded"/>
+        </div>
+        <div className="absolute right-[73%] top-[790px] px-4 py-2 rounded-md border border-zinc-800 bg-zinc-900">
+          <TokenETH size={40} variant="branded"/>
+        </div>
       </div>
       <div>
         // cryto image slider
