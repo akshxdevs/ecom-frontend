4ff63d238d883a44db05699fb5d82b55406436b4
akshxdevs <akshxdevs@gmail.com>
2025-11-09 18:06:07 +0530

    update: made main changes

---
+++ app/Components/HomePage/Main.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Products, useCartLength } from "./Products";
+import { Heart, ShoppingCart } from "lucide-react";
+import localFont from "next/font/local"; 
+
+
+
+const myFont = localFont({
+  src: '../../../public/fonts/Palmore.otf',
+});
+const myFont2 = localFont({
+  src: '../../../public/fonts/PalmoreLight.ttf',
+});
+
+export const Main = () => {
+  const router = useRouter();
+  const { cartLength } = useCartLength(); 
+
+  return (
+    <div className="p-16">
+      <div className="flex justify-between py-5">
+        <div>
+          <h1 className={`${myFont.className} text-6xl`}>
+            Products
+          </h1>
+        </div>
+        <div className="flex items-center gap-3">
+          <div>
+            <button>
+              <Heart size={29}/>
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => router.push("/cart")}
+              className="text-white cursor-pointer"
+            >
+              <ShoppingCart size={29}/>
+            </button>
+            <span className="relative text-red-500 font-semibold right-3 bg-white rounded-full px-1.5 py-0.5">
+              {cartLength}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div>
+      <div className={`${myFont2.className} text-2xl mb-10 text-center text-gray-300`}>
+        <p>you’ll browse the shop, see the tee, hit buy, sign the tx, burn a little gas, own the look, love the vibe, and somehow feel completely happy</p>      
+      </div>
+      </div>
+      <Products />
+    </div>
+  );
+};
