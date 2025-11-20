31ed9ba330990dc0a115cd13a0207725dbc0c872
akshxdevs <akshxdevs@gmail.com>
2025-11-20 23:10:12 +0530

    refactor: change port to 8000

--- auth-api/src/main.rs
+++ auth-api/src/main.rs
@@ -1,22 +1,22 @@
 use std::{net::SocketAddr, sync::Arc};
 
-use axum::{Router, routing::post};
-use api::routes::{signup_route,signin_route};
+use api::routes::{signin_route, signup_route};
+use axum::{routing::post, Router};
 use store::Store;
 
 #[tokio::main]
-async fn main() ->Result<(),std::io::Error>{
+async fn main() -> Result<(), std::io::Error> {
     let store = Arc::new(Store::new().await);
-    
-    let app = Router::new()
-    .route("/signup", post(signup_route))
-    .route("/signin", post(signin_route))
-    .with_state(store);
 
-    let addr = SocketAddr::from(([0,0,0,0,],3000));
-    println!("server running on port {}",addr);
+    let app = Router::new()
+        .route("/signup", post(signup_route))
+        .route("/signin", post(signin_route))
+        .with_state(store);
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+    println!("server running on port {}", addr);
 
     let listener = tokio::net::TcpListener::bind(&addr).await?;
-    axum::serve(listener,app).await?;
+    axum::serve(listener, app).await?;
     Ok(())
 }
