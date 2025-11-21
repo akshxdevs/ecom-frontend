bc1322bba8ba1de7c876558a27e1fbcc4536a0a0
akshxdevs <akshxdevs@gmail.com>
2025-11-21 21:13:55 +0530

    refactor: major changes + withdrawl logic added

--- app/Components/Appbar.tsx
+++ app/Components/Appbar.tsx
@@ -1,12 +1,17 @@
 "use client";
 import WalletConnect from "./WalletConnect";
-import {  BellIcon, Menu,  PlusSquareIcon, SearchIcon, ShoppingBagIcon, Wallet2Icon } from "lucide-react";
-import { useWallet } from "@solana/wallet-adapter-react";
+import {  BellIcon, Dot, Menu,  PlusSquareIcon, RedoDot, SearchIcon, ShoppingBagIcon, Wallet2Icon } from "lucide-react";
+import { AnchorWallet, useWallet } from "@solana/wallet-adapter-react";
 import { Connection, clusterApiUrl } from "@solana/web3.js";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import localFont from "next/font/local"; 
-
+import { Escrow } from "ecom-sdk";
+import {PublicKey} from "@solana/web3.js"
+import { useWithdrawStore } from "../store/useWithdrawStore";
+import toast from "react-hot-toast";
+import { useVaultState } from "../store/vaultStore";
+import { useSellerState } from "../store/sellerPubkeyStore";
 
 const myFont = localFont({
     src: '/../../public/fonts/Palmore.otf',
@@ -17,9 +22,31 @@ export const Appbar = () => {
     const connection = new Connection(clusterApiUrl("devnet"));
     const [balance,setBalance] = useState<number>(0.0013);
     const router = useRouter();
+    const withdrawTrue = useWithdrawStore((s) => s.withdrawTrue);
+    const setWithdrawTrue = useWithdrawStore((s) => s.setWithdrawTrue);
+    const walletAdapter = useWallet();
+    const [notificationModel, setShowNotificationModel] = useState(false);
+    const sellerPubkey = useSellerState((s)=>s.sellerPubkey);
+    const params = useParams();
+    const orderId = params.OrderId as string; 
+    const shortId = (id: string) => id.slice(0, 8) + "...";
+    const displayId = orderId ? shortId(orderId) : "";
+    const vaultPda = useVaultState((s)=>s.vault);
+    const vaultStatePda = useVaultState((s)=>s.stateVault);
+    const [paymentPda, setPaymentPda] = useState<string | undefined>(undefined);
+    const [escrowPda, setEscrowPda] = useState<PublicKey | undefined>(undefined);
+    const [closeAccounts,setCloseAccounts] = useState(false);
+    useEffect(()=>{
+        console.log({withdrawTrue});
+    },[withdrawTrue]);
+
+    useEffect(()=>{
+        console.log({sellerPubkey});
+    },[sellerPubkey]);
+
     useEffect(()=>{
         fetchBalance();
-    },[publicKey])
+    },[publicKey]);
 
     const fetchBalance = async() => {
         try {
@@ -29,7 +56,101 @@ export const Appbar = () => {
         } catch (error) {
             
         }
+    };
+    console.log({sellerPubkey});
+    console.log({vaultPda});
+    console.log({vaultStatePda});
+    console.log({escrowPda});
+    console.log({paymentPda});
+    const withdrawlEscrow = async() => {
+        if (!sellerPubkey) return toast.error("Seller information not available");
+        console.log({sellerPubkey});
+        
+        const escrow = new Escrow(walletAdapter);
+        try {
+            if (!vaultStatePda) return;
+            if (!vaultPda) return;
+            console.log({vaultPda});
+            console.log({vaultStatePda});
+            console.log({escrowPda});
+            console.log({paymentPda});
+            const result = await escrow.initEscrowWithdraw(
+                1,
+                walletAdapter as AnchorWallet,
+                new PublicKey(sellerPubkey),
+                new PublicKey(vaultStatePda),
+                new PublicKey(vaultPda),
+            );
+            if (result.success) {
+                toast.success("Withdrwal success!");
+                if (result.paymentPda) setPaymentPda(result.paymentPda);
+                if (result.escrowPda) setEscrowPda(new PublicKey(result.escrowPda));
+                setWithdrawTrue(false);
+                setCloseAccounts(true);
+            };
+        } catch (error) {
+            toast.error((error as Error).message);
+            closeAccount();
+            console.error(error);
+        }
     }
+
+    const closeAccount = async() => {
+        const escrow = new Escrow(walletAdapter);
+        if (!paymentPda || !escrowPda || !vaultPda || !vaultStatePda) throw new Error("PDA messing...")
+        const result = await escrow.closeAccounts(
+          walletAdapter as AnchorWallet,
+          new PublicKey(paymentPda),
+          new PublicKey(escrowPda),
+          new PublicKey(vaultStatePda),
+          new PublicKey(vaultPda),
+        );
+        if (result.success) {
+          toast.success("PDA's closed successfully..")
+        }else{
+          console.log(result.error);
+          toast.error("Failed to closed Account");
+        }
+      }
+      const closeOrder = async() => {
+        const escrow = new Escrow(walletAdapter);
+        if (!paymentPda || !escrowPda || !vaultPda || !vaultStatePda) throw new Error("PDA messing...")
+        const result = await escrow.closeOrder(
+          walletAdapter as AnchorWallet,
+        );
+        if (result.success) {
+          toast.success("PDA's closed successfully..")
+        }else{
+          console.log(result.error);
+          toast.error("Failed to closed Account");
+        }
+      }
+
+      useEffect(() => {
+        if (!closeAccounts) new Error("Not ready yet!"); 
+        let elapsed = 0;
+        const interval = setInterval(() => {
+          // Check every 500ms
+          elapsed += 500;
+      
+          // If all values exist → run the function
+          if (paymentPda && escrowPda && vaultPda && vaultStatePda) {
+            closeAccount();
+            clearInterval(interval);
+          }
+      
+          // Stop after 30 seconds
+          if (elapsed >= 30000) {
+            console.log("Timeout: PDAs not found in 30s");
+            clearInterval(interval);
+          }
+      
+        }, 500);
+      
+        return () => clearInterval(interval); // cleanup
+      }, [paymentPda, escrowPda, vaultPda, vaultStatePda]);
+      
+
     return (
             <div className="max-w-6xl w-full mx-auto p-2">
                 <div className="flex justify-between items-center gap-16">
@@ -61,7 +182,25 @@ export const Appbar = () => {
                         <div>
                             <WalletConnect/>
                         </div>
-                        <div className="border p-2 rounded-md border-zinc-800 hover:border-blue-900">
+                        <div onClick={()=> setShowNotificationModel((prev)=>!prev)} className="flex border pl-2 pr-3 py-2 rounded-md border-zinc-800 hover:border-blue-900">
+                            <div className="absolute top-0">
+                                {withdrawTrue ? (
+                                    <Dot size={40} className="text-red-500"/>
+                                ): ""}
+                            </div>
+                            {notificationModel && (
+                                <div className="absolute right-32 top-12 bg-zinc-900 py-5 border border-zinc-600 rounded-xl px-4">
+                                    <div className="flex justify-between gap-4">
+                                        <div>
+                                            <h1 className="text-sm">Order ID</h1>
+                                            <p className="text-xs">{displayId}</p>
+                                        </div>
+                                        <div>
+                                            <button className="w-20 cursor-pointer px-2 py-1 bg-white rounded-xl text-black text-xs" onClick={withdrawlEscrow}>Withdraw Funds</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
                             <BellIcon size={18}/>
                         </div>
                         <div className="border p-2 rounded-md border-zinc-800 hover:border-blue-900">
